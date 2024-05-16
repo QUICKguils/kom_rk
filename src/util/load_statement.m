c37560188ce4248c9e59aa5a9353eea48fe9d811
QUICKguils <guilainsjc@gmail.com>
2024-05-16 16:25:29 +0200

    Add performance requirements

--- src/util/load_statement.m
+++ src/util/load_statement.m
@@ -4,17 +4,16 @@ function Stm = load_statement
 % This function returns a structure that contains the data provided by
 % the statement, which are used throughout the project.
 
-% TODO: add performance requirements
-
 % Millennium Falcon
 Stm.Falcon.diameter = 20;     % Diameter [m]
+Stm.Falcon.radius   = 10;     % Radius [m]
 Stm.Falcon.height   = 8;      % Height [m]
 Stm.Falcon.mass     = 100e3;  % Mass (without control system) [kg]
 
 % Requirement on the control system
-Stm.Ctrl.massMax    = 10e3;   % Max. mass [kg]
-Stm.Ctrl.powerMax   = 1e6;    % Max. input power [W]
-Stm.Ctrl.voltageMax = 100e3;  % Max. applied voltage [V]
+Stm.Acs.massMax    = 10e3;   % Max. mass [kg]
+Stm.Acs.powerMax   = 1e6;    % Max. input power [W]
+Stm.Acs.voltageMax = 100e3;  % Max. applied voltage [V]
 
 % Reaction wheels
 Stm.RW.beta     = deg2rad(63.4);  % Elevation angle of the pyramid [rad]
@@ -25,7 +24,18 @@ Stm.RW.damping   = 1e-4;          % Damping factor [N*m*s]
 Stm.RW.density   = 8e3;           % Density of steel composing the wheels [kg/m³]
 
 % Thrusters
-Stm.Thruster.thrust = 1e3;  % Thrust [N]
-% TODO: add more if we ineed use thrusters.
+% TODO: add thrusters statement data if we need to use them
+
+% Performance requirements
+Stm.Perf.Roll.angle         = deg2rad(90);  % Roll rotation angle [rad]
+Stm.Perf.Roll.PO            = 20;           % Percentage overshoot
+Stm.Perf.Roll.settlingTime  = 10;           % Settling time [s]
+Stm.Perf.Roll.SettlingRtol  = 0.1;          % Settling relative tolerance
+Stm.Perf.Pitch.angle        = deg2rad(30);  % Pitch rotation angle [rad]
+Stm.Perf.Pitch.PO           = 5;            % Percentage overshoot
+Stm.Perf.Pitch.settlingTime = 5;            % Settling time [s]
+Stm.Perf.Yaw.recoveryTime   = 5;            % Laser hit recovery time [s]
+Stm.Perf.Yaw.LaserTorque    = 4000;         % Torque induced by laser hit [N/m]
+Stm.Perf.Yaw.LaserTime      = 0.5;          % Duration of laser hit [s]
 
 end
