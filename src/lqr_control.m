f27189a9de92f322ce90302ed968e5bc7e841f95
QUICKguils <guilainsjc@gmail.com>
2024-05-22 15:50:04 +0200

    First dreaft for LQR design

--- src/lqr_control.m
+++ src/lqr_control.m
@@ -1,4 +1,16 @@
-function Lqr = lqr_control(RunArg, Stm)
+function Lqr = lqr_control(RunArg, Stm, SS)
 % LQR_CONTROL  LQR attitude control system for the Millennium Falcon.
+
+[A, B, C, D] = get_roll_ss(Stm);
+
+% Heuristically determined Q and R matrices.
+Q = [100, 0;
+     0,   1];
+R = 0.05;
+
+K = lqr(A, B, Q, R);
+
+A_cl = A - B*K;
+sys = ss(A_cl, B, C, D);
 
 end
