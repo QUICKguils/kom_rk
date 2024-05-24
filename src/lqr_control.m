f591ff242f87e29da646994d05edfed930626235
QUICKguils <guilainsjc@gmail.com>
2024-05-24 22:27:15 +0200

    non-working: step response for the LQR control

--- src/lqr_control.m
+++ src/lqr_control.m
@@ -10,19 +10,29 @@ C = SS.Roll.C;
 D = SS.Roll.D;
 
 % Heuristically determined Q and R matrices.
-Q = [100, 0;
-     0,   1];
-R = 0.05;
+Q = diag([1, 0]);  % That's the roll angle that matters here.
+R = 1e-7;
 
+% Optimal gain
 K = lqr(A, B, Q, R);
 
+% Full state feedback system
 A_cl = A - B*K;
 sys = ss(A_cl, B, C, D);
+
+timeSample = 0:0.01:Stm.Roll.settlingTime;
+target = Stm.Roll.angle;
+
+x0 = [target; 0];
+[y, t, ~] = initial(sys, x0, timeSample);
+figure("WindowStyle", "docked");
+plot(t, 1 - y/target);
 
 Lqr.Roll.Q    = Q;
 Lqr.Roll.R    = R;
 Lqr.Roll.K    = K;
 Lqr.Roll.A_cl = A_cl;
 Lqr.Roll.sys  = sys;
+
 
 end
