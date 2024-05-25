894aaaf26343178aa3168793657f96af38729f10
QUICKguils <guilainsjc@gmail.com>
2024-05-25 22:21:04 +0200

    Create quick util script to load the computed data

---
+++ src/util/refresh_workspace.m
@@ -0,0 +1,8 @@
+% REFRESH_WORKSPACE  Bring computed data in global workspace.
+
+Stm    = load("res/statement.mat");
+RunArg = load("res/runArguments.mat");
+Reqr   = load("res/requirements.mat");
+SS     = load("res/stateSpace.mat");
+Lqr    = load("res/lqrControl.mat");
+Pid    = load("res/pidControl.mat");
