f8aa47d1420ee57333128d4260ad25e359490cff
QUICKguils <guilainsjc@gmail.com>
2024-05-25 23:30:53 +0200

    Compute optimal proportional gain for all axes

--- src/lqr_control.m
+++ src/lqr_control.m
@@ -1,55 +1,82 @@
-function Lqr = lqr_control(RunArg, Stm, SS)
+function Lqr = lqr_control(RunArg, SS)
 % LQR_CONTROL  LQR attitude control system for the Millennium Falcon.
 %
 % Arguments:
-%   RunArg (struct) -- Code execution parameters, with fields:
-%     opts  (1xN char) -- Output options
-%       'p' -> Enable [P]lots creation
-%   Stm    (struct) -- Project statement data
+%   RunArg (struct) -- Code execution parameters, with field:
+%     selsim ({'roll', 'pitch', 'yaw'}) -- Select simulink model
+%       'roll'  -> Run the roll models
+%       'pitch' -> Run the pitch models
+%       'yaw'   -> Run the yaw models
 %   SS     (struct) -- State-space representations
 % Return:
-%   Reqr (struct) -- Requirements estimation, with fields:
-%     RollEvo  (struct) -- Evolution of torque, momentum and angle in roll
-%     PitchEvo (struct) -- Evolution of torque, momentum and angle in pitch
-%     YawEvo   (struct) -- Evolution of torque, momentum and angle in yaw
+%   Lqr.Roll, Lqr.Pitch, Lqr.Yaw (struct) -- LQR controllers, with fields:
+%     Q (array[double]) -- Q matrix: weight on the state vector.
+%     R (array[double]) -- R matrix: weight on the input vector.
+%     K (array[double]) -- K matrix: optimal proportional gain.
 
-% % Unpack relevant execution parameters
-% LocalRunArg = {RunArg.opts, RunArg.selsim};
-% [opts, selsim] = LocalRunArg{:};
+% TODO:
+% - run selected simulink model by Runarg.selsim
 
-%% Roll
+% Unpack relevant execution parameters
+LocalRunArg = {RunArg.selsim};
+selsim = LocalRunArg{:};
+
+% 1. Heuristics for Q and R matrices
+
+[Q_roll,  R_roll]  = heuristic_QR('roll');
+[Q_pitch, R_pitch] = heuristic_QR('pitch');
+[Q_yaw,   R_yaw]   = heuristic_QR('yaw');
+
+% 2. LQR controller for the state-space systems
+
+Lqr.Roll  = lqr_system(SS.Roll,  Q_roll,  R_roll);
+Lqr.Pitch = lqr_system(SS.Pitch, Q_pitch, R_pitch);
+Lqr.Yaw   = lqr_system(SS.Yaw,   Q_yaw,   R_yaw);
+end
+
+%% 1. Heuristics for Q and R matrices
+
+function [Q, R] = heuristic_QR(axis)
+% HEURISTIC_QR  Return the heuristics values of Q and R.
+%
+% Argument:
+%   axis ({'roll', 'pitch', 'yaw'}) -- select the axis of rotation
+
+switch axis
+	case 'roll'
+		Q = 1e13 * diag([1, 1]);
+		R = 0.001;
+	case 'pitch'
+		Q = 1e13 * diag([1, 1]);
+		R = 0.001;
+	case 'yaw'
+		Q = 1e13 * diag([1, 1]);
+		R = 0.001;
+end
+end
+
+% 2. LQR controller for the state-space systems
+
+function Lqr = lqr_system(SS_rot, Q, R)
+% LQR_system Return LQR controller for the given state-space system.
+%
+% Arguments:
 
 % Local aliases
-A = SS.Roll.A;
-B = SS.Roll.B;
-C = SS.Roll.C;
-D = SS.Roll.D;
-
-% Heuristically determined Q and R matrices.
-Q = 1e13 * diag([1, 1]);  % The rotation angle matters
-R = 0.001;
+A = SS_rot.A;
+B = SS_rot.B;
 
 % Optimal gain
 K = lqr(A, B, Q, R);
 
-% Full state feedback system
-A_cl = A - B*K;
-sys = ss(A_cl, B, C, D);
+% % Full state feedback system
+% A_cl = A - B*K;  % TODO: see if useful to compute that
+% sys = ss(A_cl, B, C, D);  % TODO: see if useful to compute that
 
-% timeSample = 0:0.01:Stm.Roll.settlingTime;
-% target = Stm.Roll.angle;
-% 
-% x0 = [target; 0];
-% [y, t, ~] = initial(sys, x0, timeSample);
-% % figure("WindowStyle", "docked");
-% % plot(t, 1 - y/target);
-
-Lqr.Roll.Q    = Q;
-Lqr.Roll.R    = R;
-Lqr.Roll.K    = K;
-Lqr.Roll.A_cl = A_cl;
-Lqr.Roll.sys  = sys;
-
-% Simulink
-
+% Build the return data structure
+Lqr.Q    = Q;
+Lqr.R    = R;
+Lqr.K    = K;
+% Lqr.A_cl = A_cl;
+% Lqr.sys  = sys;
 end
